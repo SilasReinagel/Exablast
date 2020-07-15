f901f4b9090b6aebe14c1e98a45f179e9ed49f9e
Silas Reinagel <silas.reinagel@carvana.com>
2020-07-15 16:56:10 -0700

    Dabbling with CSS Animations and Intersection Observer

--- src/Pages/Home.svelte
+++ src/Pages/Home.svelte
@@ -1,12 +1,46 @@
 <script>
 	import Hero from '../Sections/Hero.svelte';
+	import WhatWeDo from '../Sections/WhatWeDo.svelte';
+	import WorkExample from '../Sections/WorkExample.svelte';
+	import AboutUsMinimal from '../Sections/AboutUsMinimal.svelte';
+	import Clients from '../Sections/Clients.svelte';
+	import ProjectContactPitch from '../Sections/ProjectContactPitch.svelte';
 </script>
 
 <Hero/>
+
+<section class="alt-bg">
+	<WhatWeDo/>
+</section>
+
+<section>
+	<WorkExample/>
+</section>
+
+<section class="alt-bg">
+	<AboutUsMinimal/>
+</section>
+
+<section>
+	<Clients/>
+</section>
+
+<section class="alt-bg">
+	<ProjectContactPitch/>
+</section>
 
 <style>
   .darken-gradient {
     background-image: linear-gradient(rgba(23, 17, 26, 0.01) 0%, rgba(23, 17, 26, 0.6) 33%, rgba(23, 17, 26, 0.9) 100%);
     width: 100%;
-  }
+	}
+
+	section {
+		min-height: 100vh;
+	}
+
+	.alt-bg {
+		background-color: #fff;
+		width: 100%;
+	}
 </style>
---
+++ src/Sections/WorkExample.svelte
@@ -0,0 +1,9 @@
+<script>
+  import AnimatedSectionHeader from '../Elements/AnimatedSectionHeader.svelte';
+</script>
+
+<div class="py4">
+  <AnimatedSectionHeader secondary
+    header="We love taking products from vision to reality."
+    subheader=""/>
+</div>
